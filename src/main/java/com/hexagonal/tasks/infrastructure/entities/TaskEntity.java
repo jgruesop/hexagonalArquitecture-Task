685bdb23d0f16bbcd7d725974f7818116ccb1293
jgruesop <jhonper86@gmail.com>
2025-02-06 08:43:49 +0100

    Creando entidad Task

--- src/main/java/com/hexagonal/tasks/infrastructure/entities/TaskEntity.java
+++ src/main/java/com/hexagonal/tasks/infrastructure/entities/TaskEntity.java
@@ -1,4 +1,34 @@
 package com.hexagonal.tasks.infrastructure.entities;
 
+import com.hexagonal.tasks.domain.models.Task;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class TaskEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private LocalDateTime creationDate;
+    private boolean completed;
+
+    public static TaskEntity domainModel(Task task) {
+        return new TaskEntity(task.getId(), task.getTitle(), task.getDescription(), task.getCreationDate(), task.getCompleted());
+    }
+
 }
